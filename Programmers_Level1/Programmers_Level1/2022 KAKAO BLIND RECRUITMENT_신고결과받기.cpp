4b2c07d8bb64aac01010ed968088af3668a3b806
UihwanLee <36596037+UihwanLee@users.noreply.github.com>
2022-07-01 09:42:45 +0900

    Upload problem

--- Programmers_Level1/Programmers_Level1/2022 KAKAO BLIND RECRUITMENT_신고결과받기.cpp
+++ Programmers_Level1/Programmers_Level1/2022 KAKAO BLIND RECRUITMENT_신고결과받기.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <sstream>
 #include <map> 
+#include <typeinfo>
 
 using namespace std;
 
@@ -22,10 +23,10 @@ vector<string> Split(const string& text)
 }
 
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
-	vector<int> answer;
 
 	// 신고한 사람 - 신고 당한 사람 
 	map<string, vector<string>> list;
+	map<string, vector<string>> list2;
 
 	// 신고 받은 사람 리스트 정리 
 	for (const auto& report_item : report)
@@ -34,18 +35,36 @@ vector<int> solution(vector<string> id_list, vector<string> report, int k) {
 		auto& first_person = v[0];  // 신고한 사람
 		auto& second_person = v[1];  // 신고 당한 사람
 
-		if (list[first_person].empty() || (find(list[first_person].begin(), list[first_person].end(), second_person) == list[second_person].end()))
+		if (list2[second_person].empty() || (find(list2[second_person].begin(), list2[second_person].end(), first_person) == list2[second_person].end()))
 		{
-
+			list2[second_person].emplace_back(first_person);
+		}
+		if (list[first_person].empty() || (find(list[first_person].begin(), list[first_person].end(), second_person) == list[first_person].end()))
+		{
+			list[first_person].emplace_back(second_person);
 		}
 	}
 
 	// 결과 리스트 정리 
-
-	answer.push_back(1);
+	vector<int> answer;
+	
+	for(const auto& id_item : id_list)
+	{
+		auto count = 0;
+		auto& who_list = list[id_item];
+		for(const auto& who : who_list)
+		{
+			if(list2[who].size() >= k)
+			{
+				++count;
+			}
+		}
+		answer.emplace_back(count);
+	}
 
 	return answer;
 }
+
 
 int main(void)
 {
