0dd431acf94079157e5d02fe9f17b6a3703e1e30
UihwanLee <luh5063014@naver.com>
2022-11-11 23:34:29 +0900

    Upload problem

--- Level0/70_A로 B 만들기.cpp
+++ Level0/70_A로 B 만들기.cpp
@@ -1,27 +1,20 @@
-#include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <map>
 
 using namespace std;
 
-int main()
-{
-	int answer = 0;
+int solution(string before, string after) {
+    int answer = 0;
     bool check = true;
-    string before = "olleh", after="hello"; 
     sort(before.begin(), before.end());
     sort(after.begin(), after.end());
-    
-    for(int i=0; before.length(); i++)
+
+    for(int i=0; i<before.length(); i++)
     {
         if(before[i]!=after[i]) check = false;
     }
 
     answer = (check) ? 1 : 0;
-    
-    cout << answer;
-    
-    return 0;
+    return answer;
 }
