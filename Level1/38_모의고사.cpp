30e63fe30cb87338f19de46aeeea83868573ace4
UihwanLee <luh5063014@naver.com>
2022-11-29 17:50:46 +0900

    Upload problem

--- Level1/38_모의고사.cpp
+++ Level1/38_모의고사.cpp
@@ -1,24 +1,16 @@
-#include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <map>
-#include <cmath>
-#include <sstream>
 
 using namespace std;
 
-int main()
-{
-	vector<int> answers = {1, 2, 3, 4, 5};
-	vector<int> answer;
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
     int cnt_p1=0, cnt_p2=0, cnt_p3=0, cnt_max=0;
     vector<int> p1 = {1, 2, 3, 4, 5};
     vector<int> p2 = {2, 1, 2, 3, 2, 4, 2, 5};
     vector<int> p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
     
     for(int i=0; i<answers.size(); i++){
-        cout << i%p1.size() << " " << p2[i%p2.size()] << " " << p3[i%p3.size()] << " ";
         if(answers[i] == p1[i%p1.size()]) cnt_p1++;
         if(answers[i] == p2[i%p2.size()]) cnt_p2++;
         if(answers[i] == p3[i%p3.size()]) cnt_p3++;
@@ -28,7 +20,5 @@ int main()
     if(cnt_max==cnt_p1) answer.emplace_back(1);
     if(cnt_max==cnt_p2) answer.emplace_back(2);
     if(cnt_max==cnt_p3) answer.emplace_back(3);
-    //cout << answer;
-    
-    return 0;
-} 
+    return answer;
+}
