322f6fad4ff1d142b2c66a79eb2af141b23c4ef3
UihwanLee <luh5063014@naver.com>
2022-12-14 18:26:31 +0900

    Upload problem

--- Level1/56_숫자 짝궁.cpp
+++ Level1/56_숫자 짝궁.cpp
@@ -1,40 +1,20 @@
-#include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <map>
-#include <cmath>
-#include <sstream>
-#include <map>
 
 using namespace std;
 
-
-int main()
-{
-	string X = "5525", Y = "1255";
+string solution(string X, string Y) {
     string answer = "";
     vector<int> v1(11, 0), v2(11, 0); 
     
     string max, min;
     if(X.length() >= Y.length()) { max=X, min=Y; }
     else { max=Y, min=X; }
-    //string max = X.length() >= Y.length() ? X : Y;
-    //string min = X.length() <= Y.length() ? X : Y;
     
     for(int i=0; i<max.length(); i++){
         if(i<min.length()) v1[min[i]-'0']++;
         v2[max[i]-'0']++;
     }
-    
-    for(int i=0; i<v2.size(); i++){
-        std::cout << v1[i];
-    }
-    std::cout << '\n';
-    for(int i=0; i<v1.size(); i++){
-        std::cout << v2[i];
-    }
-    std::cout << '\n';
     
     for(int i=v1.size()-1; i>=0; i--){
         if(v1[i]!=0 && v2[i]!=0) {
@@ -44,7 +24,6 @@ int main()
         }
     }
     answer = (answer == "") ? "-1" : (answer[0]-'0'==0) ? "0" : answer;
-    cout << answer;
-	
-	return 0;
+    
+    return answer;
 }
