0c0276cfc7adfbd7430aae94ba222d7047497670
Lee Ui Hwan <luh5063014@naver.com>
2024-08-01 21:32:38 +0900

    Upload problem

---
+++ Level0/111_문자열 겹쳐쓰기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    string answer = "";
+    
+    for(int i=0; i<s; i++) {
+        answer += my_string[i];
+    }
+    
+    int temp = s + overwrite_string.length();
+    
+    for(int i=0; i<overwrite_string.length(); i++) {
+        answer += overwrite_string[i];
+    }
+    
+    for(int i=s + overwrite_string.length(); i<my_string.length(); i++) {
+        answer += my_string[i];
+    }
+    
+    return answer;
+}
